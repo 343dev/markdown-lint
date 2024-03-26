e22b3f70db46021998b4c1986ac40938b97d505a
343dev <343dev@users.noreply.github.com>
2024-03-26 19:14:44 +0700

    Fix ESLint errors

--- cli.js
+++ cli.js
@@ -8,27 +8,27 @@ const { prepareExtensions } = require('./lib/utils');
 const { version, description } = require('./package.json');
 
 program
-  .option('--fix', 'Automatically fix problems')
-  .option('--ext <value>', 'Specify file extensions', prepareExtensions, ['md', 'MD'])
-  .option('-t, --typograph', 'Enable typograph')
-  .option('-r, --recursive', 'Get files from provided directory and the entire subtree')
-  .option('-c, --config <file>', 'Use this configuration, overriding default options if present');
+	.option('--fix', 'Automatically fix problems')
+	.option('--ext <value>', 'Specify file extensions', prepareExtensions, ['md', 'MD'])
+	.option('-t, --typograph', 'Enable typograph')
+	.option('-r, --recursive', 'Get files from provided directory and the entire subtree')
+	.option('-c, --config <file>', 'Use this configuration, overriding default options if present');
 
 program
-  .usage('[options] <dir> <file ...>')
-  .version(version, '-v, --version')
-  .description(description)
-  .parse(process.argv);
+	.usage('[options] <dir> <file ...>')
+	.version(version, '-v, --version')
+	.description(description)
+	.parse(process.argv);
 
-if (!program.args.length) {
-  program.help();
+if (program.args.length === 0) {
+	program.help();
 } else {
-  markdownLint({
-    paths: program.args,
-    fix: program.fix,
-    ext: program.ext,
-    recursive: program.recursive,
-    config: program.config,
-    typograph: program.typograph,
-  });
+	markdownLint({
+		paths: program.args,
+		fix: program.fix,
+		ext: program.ext,
+		recursive: program.recursive,
+		config: program.config,
+		typograph: program.typograph,
+	});
 }
--- index.js
+++ index.js
@@ -8,37 +8,36 @@ const { getFilesByPath } = require('./lib/utils');
 const appConfig = require('./.markdownlintrc');
 
 function markdownLint({ paths = [], fix, ext, recursive, config, typograph }) {
-  const dirs = paths.filter(p => fs.existsSync(p) && fs.statSync(p).isDirectory());
-  const extensions = ext.join('|');
-  const extensionsRegExp = new RegExp(`.+\\.(${extensions})$`, 'i');
+	const directories = paths.filter(p => fs.existsSync(p) && fs.statSync(p).isDirectory());
+	const extensions = ext.join('|');
+	const extensionsRegExp = new RegExp(`.+\\.(${extensions})$`, 'i');
 
-  const files = paths
-    .filter(p => extensionsRegExp.test(p) && fs.existsSync(p) && fs.statSync(p).isFile())
-    .concat(...(dirs.map(d => getFilesByPath(d, extensions, recursive))));
+	const files = [
+		...paths.filter(p => extensionsRegExp.test(p) && fs.existsSync(p) && fs.statSync(p).isFile()),
+		...directories.flatMap(d => getFilesByPath(d, extensions, recursive)),
+	];
 
-  // eslint-disable-next-line import/no-dynamic-require
-  const externalConfig = config && require(path.resolve(config));
+	const externalConfig = config && require(path.resolve(config));
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const filePath of files) {
-    let fileContent = fs.readFileSync(filePath, 'utf8');
+	for (const filePath of files) {
+		let fileContent = fs.readFileSync(filePath, 'utf8');
 
-    if (fix) {
-      fileContent = fixFile(fileContent, {
-        appConfig,
-        externalConfig,
-        typograph,
-      });
+		if (fix) {
+			fileContent = fixFile(fileContent, {
+				appConfig,
+				externalConfig,
+				typograph,
+			});
 
-      fs.writeFileSync(filePath, fileContent);
-    }
+			fs.writeFileSync(filePath, fileContent);
+		}
 
-    lintFile(fileContent, {
-      appConfig,
-      externalConfig,
-      filePath,
-    });
-  }
+		lintFile(fileContent, {
+			appConfig,
+			externalConfig,
+			filePath,
+		});
+	}
 }
 
 module.exports = markdownLint;
