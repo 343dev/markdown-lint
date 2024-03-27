060dbe6110e31a6e2bd453d3516b181b8fdf8bb2
343dev <343dev@users.noreply.github.com>
2024-03-27 23:31:28 +0700

    Remove glob

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
+const { fdir: Fdir } = require('fdir');
+
 const fs = require('node:fs');
 const path = require('node:path');
 
 const fixFile = require('./lib/fix');
 const lintFile = require('./lib/lint');
-const { getFilesByPath } = require('./lib/utils');
 
 const appConfig = require('./.markdownlintrc');
 
-function markdownLint({ paths = [], fix, ext, recursive, config, typograph }) {
-	const directories = paths.filter(p => fs.existsSync(p) && fs.statSync(p).isDirectory());
-	const extensions = ext.join('|');
-	const extensionsRegExp = new RegExp(`.+\\.(${extensions})$`, 'i');
+function isFileExtnameAllowed(filePath, extensions) {
+	const extname = path.extname(filePath)
+		.toLocaleLowerCase()
+		.slice(1);
 
-	const files = [
-		...paths.filter(p => extensionsRegExp.test(p) && fs.existsSync(p) && fs.statSync(p).isFile()),
-		...directories.flatMap(d => getFilesByPath(d, extensions, recursive)),
-	];
+	return extname && extensions.includes(extname);
+}
+
+function markdownLint({ paths = [], fix, ext, recursive, config, typograph }) {
+	const extensions = ext.join('|');
+
+	const directoryFilesCrawler = new Fdir()
+		.filter(filePath => isFileExtnameAllowed(filePath, extensions))
+		.withMaxDepth(recursive ? undefined : 0)
+		.withBasePath();
+
+	const filePaths = [...new Set(
+		paths.reduce((accumulator, filePath) => {
+			if (fs.existsSync(filePath)) {
+				const isDirectory = fs.statSync(filePath).isDirectory();
+
+				// search for files recursively inside the directory
+				if (isDirectory) {
+					accumulator = [
+						...accumulator,
+						...directoryFilesCrawler.crawl(filePath).sync(),
+					];
+				}
+
+				// filter files by extension
+				if (!isDirectory && isFileExtnameAllowed(filePath, extensions)) {
+					accumulator.push(filePath);
+				}
+			}
+
+			return accumulator;
+		}, []),
+	)];
 
 	const externalConfig = config && require(path.resolve(config));
 
-	for (const filePath of files) {
+	for (const filePath of filePaths) {
 		let fileContent = fs.readFileSync(filePath, 'utf8');
 
 		if (fix) {
